aeab067e45f6bbdf7e6ef7bed9588859e9f4e81f
Pavlo Kushneryk <pavlo.kushneryk@solidgate.com>
2026-03-31 14:21:16 +0200

    test(01-01): add failing transport contract alignment test
    - add TestArxivServiceContract to enforce proto/handler RPC alignment
    - fail when proto advertises RPCs outside phase-1 explicit handler methods

---
+++ internal/server/grpc/arxiv_contract_test.go
@@ -0,0 +1,19 @@
+package grpc
+
+import (
+	"testing"
+
+	arxivv1 "github.com/KushnerykPavel/go-rag-arxiv/internal/gen/arxiv/v1"
+)
+
+func TestArxivServiceContract(t *testing.T) {
+	explicitHandlerMethods := map[string]struct{}{
+		"Search": {},
+	}
+
+	for _, method := range arxivv1.ArxivService_ServiceDesc.Methods {
+		if _, ok := explicitHandlerMethods[method.MethodName]; !ok {
+			t.Fatalf("proto declares unimplemented RPC method: %s", method.MethodName)
+		}
+	}
+}
